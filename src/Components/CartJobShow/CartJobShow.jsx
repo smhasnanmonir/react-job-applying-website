dadfc0451108f205aec2d467270ca4d5805f25f1
S.M Hasnan Monir <114436561+smhasnanmonir@users.noreply.github.com>
2023-04-14 20:37:34 +0600

    Add files via upload

---
+++ src/Components/CartJobShow/CartJobShow.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const CartJobShow = ({ job }) => {
+  const { id, company_name, logo, position, salary, jobType1, jobType2 } = job;
+  return (
+    <div className="px-[250px] mb-3">
+      <div className="p-9 border-1 bg-red-300 w-[550px] rounded-lg">
+        <img className="mt-5" src={logo} alt="" srcset="" />
+        <p className="font-medium mt-5">{position}</p>
+        <p>{company_name}</p>
+        <div className="flex gap-3">
+          <p>
+            Job type: {jobType1} & {jobType2}
+          </p>
+        </div>
+        <p className="mb-2 mb-5">Salary: {salary}</p>
+        <Link
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          to={`/jobDetails/${id}`}
+        >
+          Show Details
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CartJobShow;
