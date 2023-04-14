ed265e0d6ca36a294b5f4c144d5b3827dbd44466
Hasnan <monirhasnan@gmail.com>
2023-04-14 15:41:26 +0600

    job description and details data showed

--- src/Components/JobShowMore/JobShowMore.jsx
+++ src/Components/JobShowMore/JobShowMore.jsx
@@ -17,19 +17,32 @@ const JobShowMore = () => {
     benefits,
   } = jobApply;
   return (
-    <div className="mt-9">
-      <div>
-        <div className=""></div>
-        <div className="p-11 bg-purple-200 rounded-lg w-[350px]">
-          <h1 className="mb-3 text-2xl font-extrabold">Job Details</h1>
-          <img src={logo} />
-          <p className="mt-1 font-semibold">Company name: {company_name}</p>
-          <p className="mt-1">Position: {position}</p>
-          <p className="mt-1">Salary per year: {salary}</p>
-          <p className="mt-1">
-            Job Type: {jobType1} & {jobType2}
+    <div className="mt-9 flex gap-7 lg:px-[250px] mb-[35px]">
+      <div className="w-[650px]">
+        <div className="p-9 bg-orange-200 rounded-lg">
+          <p>
+            <span className="font-bold">Job requirement: </span>
+            {requirements}
+          </p>
+          <p className="mt-5 mb-5">
+            <span className="font-bold">Description: </span>
+            {description}
+          </p>
+          <p className="">
+            <span className="font-bold">Job Benefits: </span>
+            {benefits}
           </p>
         </div>
+      </div>
+      <div className="p-11 bg-purple-200 rounded-lg w-[350px] h-[350px]">
+        <h1 className="mb-3 text-2xl font-extrabold">Job Details</h1>
+        <img src={logo} />
+        <p className="mt-1 font-semibold">Company name: {company_name}</p>
+        <p className="mt-1">Position: {position}</p>
+        <p className="mt-1">Salary per year: {salary}</p>
+        <p className="mt-1">
+          Job Type: {jobType1} & {jobType2}
+        </p>
       </div>
     </div>
   );
